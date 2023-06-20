88eabd321638fba851f98fce042e33819c20d30d
Francois Malan <78906010+Sindrei@users.noreply.github.com>
2023-06-20 09:12:18 +0200

    Added functionality to enter time
    Added event listeners to enter the time on the display as well as clear
    the display.

---
+++ script.js
@@ -0,0 +1,37 @@
+const numberButtons = document.querySelectorAll(".number-key");
+const clearButton = document.querySelector("#clear");
+
+let time = "000000";
+
+//  Event listener for numerical buttons
+numberButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    inputTime(button["value"]);
+  });
+});
+
+//  Event listener for clear button
+clearButton.addEventListener("click", () => {
+  time = "000000";
+  displayTime();
+});
+
+//  Function inputTime (value) - processes input of numerical values
+function inputTime(value) {
+  time = `${time.slice(1, 6)}${value}`;
+  displayTime();
+}
+
+/*  Function displayTime() - 
+    Retrieves the time from let time and displays it to appropriate HTML elements */
+function displayTime() {
+  const hours = document.querySelector(".hours");
+  const minutes = document.querySelector(".minutes");
+  const seconds = document.querySelector(".seconds");
+
+  hours.textContent = time.slice(0, 2);
+  minutes.textContent = time.slice(2, 4);
+  seconds.textContent = time.slice(4, 6);
+}
+
+window.onload = displayTime;
